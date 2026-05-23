385efbb7878eb3f38a99bdf6e054a77f1323858b
theawakener0 <wakeupwithme000@gmail.com>
2026-05-23 23:53:48 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/theawakener0/skynet
+
+go 1.24.0
+
+require gonum.org/v1/gonum v0.17.0 // indirect
---
+++ helper.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+
+	mt "gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat/distuv"
+)
+
+
+
+type Network struct {
+	inputs			int
+	hiddens			int 
+	outputs			int
+	hiddenWeights	*mt.Dense
+	outputWeights	*mt.Dense
+	learningRate 	float64
+}
+
+func CreateNetwork(input, hidden, output int, rate float64) (net Network) {
+	net = Network{
+		inputs: input,
+		hiddens: hidden,
+		outputs: output,
+		learningRate: rate,
+	}
+
+	net.hiddenWeights = mt.NewDense(net.hiddens, net.inputs, randomArray(net.inputs*net.hiddens, float64(net.inputs)))
+	net.outputWeights = mt.NewDense(net.outputs, net.hiddens, randomArray(net.hiddens*net.outputs, float64(net.hiddens)))
+
+	fmt.Println("[SkyNet] NerualNet created.")
+
+	return
+}
+
+func (net Network) Predict(inputData []float64) mt.Matrix {
+	inputs := mt.NewDense(len(inputData), 1, inputData)
+
+	hiddenInputs := dot(net.hiddenWeights, inputs)
+	hiddenOutputs := apply(sigmoid, hiddenInputs)
+
+	finalInputs := dot(net.outputWeights, hiddenOutputs)
+	finalOutputs := apply(sigmoid, finalInputs)
+
+	return finalOutputs
+}
+
+func (net Network) Train(inputData, targetData []float64) {
+	inputs := mt.NewDense(len(inputData), 1, inputData)
+
+	hiddenInputs := dot(net.hiddenWeights, inputs)
+	hiddenOutputs := apply(sigmoid, hiddenInputs)
+
+	finalInputs := dot(net.outputWeights, hiddenOutputs)
+	finalOutputs := apply(sigmoid, finalInputs)
+
+	targets := mt.NewDense(len(targetData), 1, targetData)
+	
+	outputErrors:= sub(targets, finalOutputs)
+	hiddenErrors := dot(net.outputWeights.T(), outputErrors)
+
+	net.outputWeights = add(net.outputWeights, scale(net.learningRate, dot(mul(outputErrors, sigmoidPrime(finalOutputs)), hiddenOutputs.T()))).(*mt.Dense)
+
+	net.hiddenWeights = add(net.hiddenWeights, scale(net.learningRate, dot(mul(hiddenErrors, sigmoidPrime(hiddenOutputs)), inputs.T()))).(*mt.Dense)
+
+}
+
+func save(net Network) {
+	h, err := os.Create("data/hweights.model")
+	defer h.Close()
+
+	if err == nil {
+		net.hiddenWeights.MarshalBinaryTo(h)
+	}
+
+	o, err := os.Create("data/oweights.model")
+	defer o.Close()
+
+	if err == nil {
+		net.outputWeights.MarshalBinaryTo(o)
+	}
+}
+
+func load(net Network) {
+	h, err := os.Open("data/hweights.model")
+	defer h.Close()
+
+	if err == nil {
+		net.hiddenWeights.Reset()
+		net.hiddenWeights.UnmarshalBinaryFrom(h)
+	}
+
+	o, err := os.Open("data/oweights.model")
+	defer o.Close()
+
+	if err == nil {
+		net.outputWeights.Reset()
+		net.outputWeights.UnmarshalBinaryFrom(o)
+	}
+}
+
+
+
+func dot(m, n mt.Matrix) mt.Matrix {
+	r, _ := m.Dims()
+	_,c := n.Dims()
+
+	o := mt.NewDense(r, c, nil)
+	o.Product(m, n)
+
+	return o
+}
+
+func apply(fn func(i, j int , v float64) float64 , m mt.Matrix) mt.Matrix {
+	r, c := m.Dims()
+
+	o := mt.NewDense(r, c, nil)
+	o.Apply(fn, m)
+
+	return o
+}
+
+func scale(s float64, m mt.Matrix) mt.Matrix {
+	r, c := m.Dims()
+
+	o := mt.NewDense(r, c, nil)
+	o.Scale(s, m)
+
+	return o
+}
+
+
+func mul(m, n mt.Matrix) mt.Matrix {
+	r, _ := m.Dims()
+	_,c := n.Dims()
+
+	o := mt.NewDense(r, c, nil)
+	o.MulElem(m, n)
+
+	return o
+}
+
+
+func add(m, n mt.Matrix) mt.Matrix {
+	r, _ := m.Dims()
+	_,c := n.Dims()
+
+	o := mt.NewDense(r, c, nil)
+	o.Add(m, n)
+
+	return o
+}
+
+func sub(m, n mt.Matrix) mt.Matrix {
+	r, _ := m.Dims()
+	_,c := n.Dims()
+
+	o := mt.NewDense(r, c, nil)
+	o.Sub(m, n)
+
+	return o
+}
+
+func addScalar(i float64, m mt.Matrix) mt.Matrix {
+	r, c := m.Dims()
+
+	a := make([]float64, r*c)
+	for k := range a {
+		a[k] = i
+	}
+
+	n := mt.NewDense(r, c, a)
+
+	return add(m, n)
+}
+
+func randomArray(size int, v float64) (data []float64) {
+	dist := distuv.Uniform{
+		Min: -1 / math.Sqrt(v),
+		Max: 1 / math.Sqrt(v),
+	}
+
+	data = make([]float64, size)
+	for i := range data {
+		data[i] = dist.Rand()
+	}
+
+	return
+}
+
+func sigmoid(i, j int, v float64) float64 {
+	return 1 / 1 + math.Exp(-1*v)
+}
+
+func sigmoidPrime(m mt.Matrix) mt.Matrix {
+	rows, _ := m.Dims()
+
+	o := make([]float64, rows)
+	for i := range o {
+		o[i] = 1
+	}
+
+	ones := mt.NewDense(rows, 1, o)
+
+	return mul(m, sub(ones, m))
+}
+
+
+
